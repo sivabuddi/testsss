4766be4691762585463ef800398cfd26e6e4d89f
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-09-23 22:54:47 -0500

    Init word conversion

--- src/main/java/okg/test/Testing.java
+++ src/main/java/okg/test/Testing.java
@@ -2,8 +2,32 @@ package okg.test;
 
 public class Testing {
 
+    public static String convert(String arr){
+        String result="";
+        for(char ch:arr.toCharArray()){
+
+            if(Character.isAlphabetic(ch)) {
+
+                if (Character.isUpperCase(ch))
+                    result += Character.toLowerCase(ch);
+                else if (Character.isLowerCase(ch))
+                    result += Character.toUpperCase(ch);
+                else
+                    result += ch;
+
+            }
+            else
+                result+=ch;
+
+        }
+        return result;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("hellow");
+        String string="Hello World Siva";
+        String result = convert(string); // hELLO wORLD sIVA
+        System.out.println(result);
 
     }
 }
